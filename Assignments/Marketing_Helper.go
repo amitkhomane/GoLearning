613cf58fd322e143657dc15c49354dd4d72ceca3
amitkhomane <amit@infracloud.io>
2019-09-05 18:39:47 +0530

    added code to check builds in betwen the time

--- Assignments/Marketing_Helper.go
+++ Assignments/Marketing_Helper.go
@@ -1,62 +1,40 @@
 package main
-/*
+
 import (
-	"fmt"
-	"io/ioutil"
-	"flag"
-)
-
-func main(){
-	fptr := flag.String("fpath", "data", "file path to csv file")
-	flag.Parse()
-	//data, err := ioutil.ReadFile("data.csv")
-	data, err := ioutil.ReadFile(*fptr)
-	if err !=nil{
-		fmt.Println("Error in reading file", err)
-		return
-	}
-	fmt.Println("Contents of the file", string(data))
-
-
-} */
-import (
-	"fmt"
-	"flag"
 	"bufio"
-	"os"
-	"time"
 	"encoding/csv"
+	"flag"
+	"fmt"
 	"io"
+	"os"
 	"sort"
+	"time"
 )
 
-
-type keyVal struct{
+type keyVal struct {
 	key string
 	val int
-
-} 
+}
 type Data struct {
-    ID   string
-	User string
+	ID          string
+	User        string
 	RequestedAt string
-	StartedAt string
-	FinishedAt string
-	Deleted string
-	ExitCode string
-	Size string
-
+	StartedAt   string
+	FinishedAt  string
+	Deleted     string
+	ExitCode    string
+	Size        string
 }
 
-func count(d [] Data)map[string]int{
+func count(d []Data) map[string]int {
 	UserCount := make(map[string]int)
-	for _, n := range d{
+	for _, n := range d {
 		user := n.User
 		_, present := UserCount[user]
-		if present{
-			UserCount[user] +=1
-		}else{
-			UserCount[user] =1
+		if present {
+			UserCount[user]++
+		} else {
+			UserCount[user] = 1
 		}
 
 	}
@@ -64,62 +42,68 @@ func count(d [] Data)map[string]int{
 
 }
 
+func inTimeSpan(start, end, check time.Time) bool {
+	return check.After(start) && check.Before(end)
+}
 
+func totalBuilds(duration string, start string, end string, data []Data) {
+	fmt.Println("In side totalBuilds", duration)
+	if duration != "" {
+		d, _ := time.ParseDuration(duration)
+		startTime := time.Now().Add(-d)
+		fmt.Println("startTime", startTime)
+		endTime := time.Now()
+		fmt.Println("endTime", endTime)
 
-// func total_builds(start, end, duration tim.Time){
+		done := make(chan bool, 1)
+		go func() {
+			count := 0
+			for _, ele := range data {
 
-// 	start := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
-// 	end := time.Date(2000, 1, 1, 0, 16, 0, 0, time.UTC)
+				startedat, _ := time.Parse(time.RFC3339, ele.StartedAt)
+				endat, _ := time.Parse(time.RFC3339, ele.FinishedAt)
+				if inTimeSpan(startTime, endTime, time.Time(startedat)) && inTimeSpan(startTime, endTime, time.Time(endat)) {
+					fmt.Println("ID: ", ele.ID)
+					count++
+				} else {
+					fmt.Println("NOT")
 
-// 	difference := end.Sub(start)
-// 	min, _ := time.ParseDuration("15m")
-// 	fmt.Printf("difference = %v\n", difference)
-// 	if min >= difference{
-// 		fmt.Printf("diff nad min are same = %v\n", difference)
-// 	}
+				}
+			}
+			fmt.Printf("Total %d builds executed in given time ", count)
+			done <- true
 
+		}()
+		<-done
 
+	}
 
-// }
+}
 
-//func total_builds(start, end time.Time)
- 
-// type Data struct{
-// 	ID 
-// }
-
-func main(){
+func main() {
 	fptr := flag.String("fpath", "data.csv", "path to csv file")
-	cptr := flag.Int("top", 1, "path to csv file")
+	cptr := flag.Int("top", 0, "list top users")
+	dptr := flag.String("d", "15m", "time duration window")
+	fromptr := flag.String("from", "", "time from")
+	toptr := flag.String("to", "", "time till")
+
 	flag.Parse()
-	
 
 	f, err := os.Open(*fptr)
-	if err !=nil{
+	if err != nil {
 		fmt.Println("Unable to read the file", err)
 		return
 	}
 
-	// defer func(){
-	// 	if err = f.Close();err !=nil{
-	// 		fmt.Println("Unable to close the file")
-	// 	}
+	defer func() {
+		if err = f.Close(); err != nil {
+			fmt.Println("Unable to close the file")
+		}
 
-	// }()
+	}()
 
-	// s := bufio.NewScanner(f)
-	// //min, _ := time.ParseDuration("15m")
-	// s1:="2018-11-29T08:30:20-05:00"
-	// t, _ := time.Parse(time.RFC3339, s1)
-	
-	// fmt.Println("duration", t)
-	// bb()
-	// for s.Scan(){
-	// 	//fmt.Println(s.Text())
-
-	// }
 	reader := csv.NewReader(bufio.NewReader(f))
-	var data []Data 
+	var data []Data
 	for {
 		line, error := reader.Read()
 		if error == io.EOF {
@@ -127,47 +111,51 @@ func main(){
 		}
 
 		data = append(data, Data{
-			ID : line[0],
-			User :line[1],
+			ID:          line[0],
+			User:        line[1],
 			RequestedAt: line[2],
-			StartedAt  : line[3],
-			FinishedAt : line[4],
-			Deleted  :line[5],
-			ExitCode :line[6],
-			Size     :line[7],
+			StartedAt:   line[3],
+			FinishedAt:  line[4],
+			Deleted:     line[5],
+			ExitCode:    line[6],
+			Size:        line[7],
 		})
-		
+
 	}
-	
+
 	fmt.Println(time.Parse(time.RFC3339, data[0].StartedAt))
 
-	user_count:= count(data)
-	fmt.Println(user_count)
-	users := make([]string, 0, len(user_count))
-	func(){
-		
-		for k:= range user_count{
+	userCount := count(data)
+	fmt.Println(userCount)
+	users := make([]string, 0, len(userCount))
+	func() {
+
+		for k := range userCount {
 			users = append(users, k)
-		} 
+		}
 	}()
 
 	var kv []keyVal
 
-	for k, v := range user_count{
+	for k, v := range userCount {
 		kv = append(kv, keyVal{k, v})
 
 	}
 
-	sort.Slice(kv, func(i, j int) bool{
+	sort.Slice(kv, func(i, j int) bool {
 		return kv[i].val > kv[j].val
 	})
-	for _, v :=range kv{
+	for _, v := range kv {
 		fmt.Printf("%s, %d\n", v.key, v.val)
-		*cptr --
-		if *cptr == 0{break}
+		*cptr--
+		if *cptr == 0 {
+			break
+		}
 
 	}
 
-
-
+	// Duration calculation
+	if *dptr != "" || *fromptr != "" || *toptr != "" {
+		totalBuilds(*dptr, *fromptr, *toptr, data)
+	}
 }
